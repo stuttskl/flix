2789bda184f6909686a8ca2c6443f6a78590af1d
Katie <25576005+stuttskl@users.noreply.github.com>
2020-09-29 09:47:07 -0700

    added movie details page

---
+++ MovieGridViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieGridViewController.swift
+//  
+//
+//  Created by Katie Stutts on 9/28/20.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MoviewGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviewGridViewController.swift
+//  flixster
+//
+//  Created by Katie Stutts on 9/28/20.
+//  Copyright © 2020 Katie Stutts. All rights reserved.
+//
+
+import UIKit
+
+class MoviewGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
